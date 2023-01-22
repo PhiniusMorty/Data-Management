3b689553a726782894bcb75c174a8ebb883a7885
PhiniusMorty <55132400+PhiniusMorty@users.noreply.github.com>
2023-01-22 16:14:10 +0530

    Update dynamic query.sql

--- Redshift/dynamic query.sql
+++ Redshift/dynamic query.sql
@@ -2,6 +2,6 @@
 
 Select 'begin; Insert into public.[table_name] ([column_name]) Select \'' + table_schema + '.' + table_name + '.' + column_name + '\' as referenceName, ' + '\'' + Case when data_type in ('integer', 'smallint','bigint','numeric') then 'sum_column' 
 else 'count rows' END + '\'' + ' as Metric_operation, ' + 'count(1) as Total_rows_table_redshift ' + 'from ' + table_schema + '.' + '"' + table_name + '";' + 'commit;' as Query
-from information_schema.column C
+from information_schema.column
 where (table_schema like '[placeholder]')
 order by table_name asc, column_name asc;
