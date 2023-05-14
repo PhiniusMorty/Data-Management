6560faccbaedcda7fe8fdd092bae31bb91703d8d
PhiniusMorty <55132400+PhiniusMorty@users.noreply.github.com>
2023-05-14 13:04:56 +0530

    Update tiktokExport.py
    Script to get data from TikTok Ads

--- Python/tiktokExport.py
+++ Python/tiktokExport.py
@@ -15,8 +15,21 @@ def getAds(**kwargs):
   # For disaster management
   start_time = f'{datetime.today() - timedelta(days = 3): %Y-%m-%d}'
   end_time = f'{datetime.today() - timedelta(days = 0): %Y-%m-%d}'
-   url = '' #Add url with parameters
+   url = 'URL' #Add url with parameters
   for i in range(1,10):
-    response = requests.get(url, headers = header
+    response = requests.get(url, headers = header)
+    dict = response.json()
+    #print(dict)
+    data_dict = dict['data']['list']    
+    for i in range(0, len(data_dict)):
+      dim_dict = data_dic[i]['dimensions']
+      metric_dict = data_dic[i]['metrics']
+      for key, value in metric_dict.items():
+        dim_dict[key] = value
+      list_tiktok.append(dim_dict)
+  #Generate a dataframe
+  df = pd.DataFrame(list_tiktok)
+  col_names = ','.join(df.columns.to_list())
+  return
   
             
